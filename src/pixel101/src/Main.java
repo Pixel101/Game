496c0d8a14fe1348ed2bc6beb068a9fbb24aa69e
Ben <bencrawford101@gmail.com>
2013-03-30 09:28:11 +0000

    graphics
    ermagerd graphics

--- src/pixel101/src/Main.java
+++ src/pixel101/src/Main.java
@@ -4,11 +4,15 @@ import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.BasicGame;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
 import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 
 public class Main extends BasicGame
 {
+	
+	Image temp;
+	float x = 0;
 
 	public Main(String title)
 	{
@@ -17,16 +21,28 @@ public class Main extends BasicGame
 
 	public void init(GameContainer container) throws SlickException
 	{
-		
+		container.setFullscreen(false);
+		temp = new Image("res/tex/tiles/temp.png");
 	}
 
 	public void update(GameContainer container, int delta) throws SlickException
 	{
-		
+		if (container.getInput().isKeyDown(Input.KEY_ESCAPE))
+		{
+			container.exit();
+		}
+		x += delta * 0.02;
 	}
 	
 	public void render(GameContainer container, Graphics g) throws SlickException
 	{
+		for (int x = 0; x < 16; x++)
+		{
+			for (int y = 0; y < 16; y++)
+			{
+				g.drawImage(temp, this.x + (x * 16), y * 16);
+			}
+		}
 		g.drawString("Hello World - Press Space!  Input is so easy!", 20, 30);
 		g.drawString("" + container.getInput().isKeyDown(Input.KEY_SPACE), 20, 50);
 	}
@@ -44,5 +60,4 @@ public class Main extends BasicGame
 			e.printStackTrace();
 		}
 	}
-
 }
