83f5c4ce76adc8278f8626ae8fe55b14fe3708c3
Ben <bencrawford101@gmail.com>
2013-03-30 09:10:56 +0000

    Now Using Slick2D
    no need for Input class, slick handles it
    Main now extends BasicGame

--- src/pixel101/src/Main.java
+++ src/pixel101/src/Main.java
@@ -1,117 +1,48 @@
 package pixel101.src;
 
-import java.awt.BorderLayout;
-import java.awt.Canvas;
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Graphics2D;
-import java.awt.image.BufferStrategy;
-import javax.swing.JFrame;
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Input;
+import org.newdawn.slick.SlickException;
 
-public class Main extends Canvas
+public class Main extends BasicGame
 {
-	private static final long serialVersionUID = 1L;
-	public static Dimension screensize = new Dimension(800, 600);
-	public boolean running = true;
-	
-	Input input;
-	
-	public Main()
+
+	public Main(String title)
 	{
-		input = new Input();
+		super(title);
 	}
-	
-	public void run()
+
+	public void init(GameContainer container) throws SlickException
 	{
-		long lastTime = System.nanoTime();
-		double unprocessed = 0;
-		double nsPerTick = 1000000000.0 / 60;
-		int frames = 0;
-		int ticks = 0;
-		long lastTimer1 = System.currentTimeMillis();
-		long now;
-
-		//try {
-			init();
-		//} catch (IOException e1) {
-		//	e1.printStackTrace();
-		//}
-
-		while (running) {
-			now = System.nanoTime();
-			unprocessed += (now - lastTime) / nsPerTick;
-			lastTime = now;
-			while (unprocessed >= 1) {
-				ticks++;
-				tick();
-				unprocessed -= 1;
-			}
-
-			try {
-				Thread.sleep(2);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-
-			frames++;
-			render();
-
-			if (System.currentTimeMillis() - lastTimer1 > 1000) {
-				lastTimer1 += 1000;
-				System.out.println(ticks + " ticks, " + frames + " fps");
-				frames = 0;
-				ticks = 0;
-			}
-		}
+		
 	}
-	
-	public void init()
+
+	public void update(GameContainer container, int delta) throws SlickException
 	{
 		
 	}
 	
-	public void tick()
+	public void render(GameContainer container, Graphics g) throws SlickException
 	{
-		
+		g.drawString("Hello World - Press Space!  Input is so easy!", 20, 30);
+		g.drawString("" + container.getInput().isKeyDown(Input.KEY_SPACE), 20, 50);
 	}
-	
-	public void render()
-	{
-		BufferStrategy bs = getBufferStrategy();
-		if (bs == null)
-		{
-			createBufferStrategy(3);
-			requestFocus();
-			return;
-		}
-		Graphics2D graphics = (Graphics2D)bs.getDrawGraphics();
-		graphics.setBackground(Color.black);
-		graphics.clearRect(0, 0, screensize.width, screensize.height);
-		
-		
-		
-		graphics.dispose();
-		bs.show();
-	}
-	
-	
-	
+
 	public static void main(String[] args)
 	{
-		Main g = new Main();
-		g.setMinimumSize(screensize);
-		g.setPreferredSize(screensize);
-		g.setMaximumSize(screensize);
-		
-		JFrame f = new JFrame();
-		f.setTitle("Hey look a game!");
-		f.setResizable(false);
-		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		f.add(g, BorderLayout.CENTER);
-		f.pack();
-		f.setLocationRelativeTo(null);
-		f.setVisible(true);
-		
-		g.run();
+		Main main = new Main("Game");
+		try
+		{
+			AppGameContainer container = new AppGameContainer(main);
+			container.start();
+		}
+		catch (SlickException e)
+		{
+			e.printStackTrace();
+		}
 	}
+
 }
