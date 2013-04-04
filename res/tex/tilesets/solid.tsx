06353799705771b3e1eb8b624e1c29ce8f70c81e
Callum <callum.pixelate@gmail.com>
2013-04-04 03:00:10 +0100

    Added floor textures
    What the fuck you reading this for... Idiot.

--- res/tex/tilesets/solid.tsx
+++ res/tex/tilesets/solid.tsx
@@ -1,13 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
-
 <tileset name="solid" tilewidth="16" tileheight="16">
-
-	<properties>
-
-		<property name="solid" value="true"/>
-
-	</properties>
-
-	<image source="../tex/tilesets/solid.png" width="128" height="128"/>
-
+ <properties>
+  <property name="solid" value="true"/>
+ </properties>
+ <image source="solid.png" width="128" height="128"/>
 </tileset>
