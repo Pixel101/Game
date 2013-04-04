d9597e4124369efad29cbfd6260882160e7d5fc3
Ben <bencrawford101@gmail.com>
2013-04-04 19:06:03 +0100

    removed solid.tsx
    no longer needed

--- res/tex/tilesets/solid.tsx
+++
@@ -1,7 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="solid" tilewidth="16" tileheight="16">
- <properties>
-  <property name="solid" value="true"/>
- </properties>
- <image source="solid.png" width="128" height="128"/>
-</tileset>
