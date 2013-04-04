39da046395f6c9b21f47c7cefade0874b7738c98
Callum <callum.pixelate@gmail.com>
2013-04-04 03:51:40 +0100

    new dirt tiles

--- res/tex/tilesets/solid.tsx
+++ res/tex/tilesets/solid.tsx
@@ -1,7 +1,9 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="solid" tilewidth="16" tileheight="16">
- <properties>
-  <property name="solid" value="true"/>
- </properties>
  <image source="solid.png" width="128" height="128"/>
+ <tile id="0">
+  <properties>
+   <property name="solid" value="true"/>
+  </properties>
+ </tile>
 </tileset>
