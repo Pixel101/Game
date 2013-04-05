2e770c2ba804814e2981ac13f4bfe6ea61b5f99a
Ben <bencrawford101@gmail.com>
2013-04-05 19:58:26 +0100

    updated .gitignore
    no longer tracks .project, .classpath

---
+++ res/tex/tilesets/floor.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+
+<tileset name="floor" tilewidth="16" tileheight="16">
+
+	<image source="../tex/tilesets/floor.png" width="128" height="128"/>
+
+</tileset>
